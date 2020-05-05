3c8b65305a9961d9ebd866d852d84afa7b3398d9
Shashang Bhagat <shbhagat@bestpeers.com>
2020-05-05 18:24:31 +0530

    configure router and header

---
+++ src/components/login/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Box} from '@material-ui/core'
+
+
+class Login extends React.Component{
+    render(){
+        return(
+            <Box color='text.secondary' p={3}>
+                <h1>Wellcome!!!</h1>
+                <h2>This is Login.</h2>
+            </Box>
+        )
+    }
+}
+
+export default Login;
---
+++ src/components/products/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Box} from '@material-ui/core'
+
+
+class Products extends React.Component{
+    render(){
+        return(
+            <Box color='text.secondary' p={3}>
+                <h1>Wellcome!!!</h1>
+                <h2>This is Products.</h2>
+            </Box>
+        )
+    }
+}
+
+export default Products;
