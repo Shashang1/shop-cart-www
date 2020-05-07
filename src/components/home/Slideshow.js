af2d39af31a011f4137892b7a9833b8dbcc4c10c
Shashang Bhagat <shbhagat@bestpeers.com>
2020-05-07 17:24:36 +0530

    create top carousel banner

---
+++ src/components/home/Slideshow.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+
+const Slideshow = ({ classes }) => {
+  const slides = [
+    {
+      imageUrl: 'https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg',
+      title: 'First slide'
+    },
+    {
+      imageUrl: 'https://mdbootstrap.com/img/Photos/Slides/img%20(46).jpg',
+      title: 'Second slide'
+    },
+    {
+      imageUrl: 'https://mdbootstrap.com/img/Photos/Slides/img%20(47).jpg',
+      title: 'Third slide'
+    }
+
+  ]
+
+  return (
+    <Carousel interval={2000} >
+      {slides.map(slide => (
+        <Carousel.Item key={slide.title} className={classes.carouselItem}>
+          <img
+            className={classes.carouselImage}
+            src={slide.imageUrl}
+            alt={slide.title}
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  )
+}
+
+export default Slideshow
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import { Box } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+
+import Slideshow from './Slideshow'
+import { styles } from './style'
 
 class Home extends React.Component {
+
   render() {
+    const { classes } = this.props
+
     return (
-      <Box color='text.secondary' p={3}>
-        <h1>Wellcome!!!</h1>
-        <h2>This is Home.</h2>
+      <Box className={classes.topContainer + ' clearfix'}>
+        <Box className={classes.carouselBox + ' float-left'}>
+          <Slideshow classes={classes} />
+        </Box>
+        <Box className={classes.textBox + ' float-left'}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book. It has survived not only five centuries, but also the leap into
+          electronic typesetting.
+        </Box>
       </Box>
     )
   }
 }
 
-export default Home
+export default withStyles(styles)(Home)
