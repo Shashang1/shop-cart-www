3c8aa4c51bdc229e9bf4f3874afe54005f9819bb
Shashang Bhagat <shbhagat@bestpeers.com>
2020-05-07 17:24:36 +0530

    create top carousel banner

---
+++ src/components/home/style.js
@@ -0,0 +1,36 @@
+export const styles = theme => ({
+  carouselBox: {
+    width: '70%',  // eslint-disable-next-line
+    ['@media (max-width:550px)']: {
+      width: '100%'
+    }
+  },
+  topContainer: {
+    width: '100%',
+    height: '200px',
+    boxSizing: 'border-box',
+    padding: theme.spacing(1)
+  },
+  carouselItem: {
+    width: '100%'
+  },
+  carouselImage: {
+    width: '100%',
+    height: '200px'
+  },
+  textBox: {
+    height: '200px',
+    width: '30%',
+    border: "1px solid black",
+    padding: theme.spacing(2),
+    fontSize: '1.22vw', // eslint-disable-next-line
+    ['@media (max-width:950px)']: {
+      fontSize: "1.4vw"
+    },
+    ['@media (max-width:550px)']: {
+      width: '100%',
+      fontSize: '3vw',
+      height: '150px'
+    }
+  }
+})
