b2c87686050ed3a2633add9424ad12fc5e4cb7fb
Shashang Bhagat <shbhagat@bestpeers.com>
2020-05-13 14:08:45 +0530

    add dummy product to redux state

--- src/redux/products/reducers.js
+++ src/redux/products/reducers.js
@@ -17,7 +17,19 @@ const initialState = {
       productName: "Tablet",
       productDescription: "The tablet with charger"
     }
-  ]
+  ],
+  currentProduct: {
+    productId: '123',
+    categoryId: '121',
+    name: 'Cell Phone',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
+      'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,' +
+      'when an unknown printer took a galley of type and scrambled it to make a type' +
+      'specimen book.It has survived not only five centuries, but also the leap into' +
+      'electronic typesetting.',
+    imageUrl: 'https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg',
+    price: 200
+  }
 }
 
 function productsReducer(state = initialState, action) {
